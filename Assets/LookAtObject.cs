fb7968ededb1fe774365a41b9afaebd49dbe252b
JuliaRon <julia@ronneberger.net>
2021-10-31 21:38:06 +0000

    update materials and add chest movement to LookAtObject

--- Assets/LookAtObject.cs
+++ Assets/LookAtObject.cs
@@ -7,10 +7,14 @@ public class LookAtObject : MonoBehaviour
 
     public GameObject objectToLookAt;
     public GameObject boneToRotate;
+    public GameObject secondBoneToRotate;
+    public GameObject referenceBone;
 
     public float verticalOffset;
 
     public float sideMultiplier = 1;
+
+    public float lerpParam = 0.1f;
 
     private void Start()
     {
@@ -23,7 +27,11 @@ public class LookAtObject : MonoBehaviour
         Vector3 lookDirection = (objectToLookAt.transform.position - boneToRotate.transform.position).normalized;
         //Vector3 lookUp = 
 
-        boneToRotate.transform.forward = lookDirection + new Vector3(0, verticalOffset, 0);
+        Vector3 wantedRotation = Vector3.Lerp(secondBoneToRotate.transform.forward, (lookDirection +referenceBone.transform.forward)/2, lerpParam);
+        secondBoneToRotate.transform.forward = wantedRotation;//new Vector3(secondBoneToRotate.transform.forward.x, secondBoneToRotate.transform.forward.y, lookDirection.z);
+        //secondBoneToRotate.transform.Rotate(new Vector3(0, lookDirection.y - secondBoneToRotate.transform.eulerAngles.y, 0));
+
+        boneToRotate.transform.forward = Vector3.Lerp(boneToRotate.transform.forward, lookDirection + new Vector3(0, verticalOffset, 0), lerpParam);
         boneToRotate.transform.RotateAround(boneToRotate.transform.forward, -objectToLookAt.transform.rotation.z * sideMultiplier);
     }
 }
