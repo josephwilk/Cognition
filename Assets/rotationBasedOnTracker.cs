9fefe9224ffbcc4960bb2c82152122d94479cdc2
JuliaRon <julia@ronneberger.net>
2021-11-11 14:47:43 +0000

    moving gears and everything following the moving..

---
+++ Assets/rotationBasedOnTracker.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationBasedOnTracker : MonoBehaviour
+{
+
+    public GameObject rotatingTracker;
+    public float rotFactor;
+    public enum Axis
+    {
+        x,y,z
+    }
+    public Axis axis;
+
+    int axisIndex;
+    float lastTrackerRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (axis == Axis.x) axisIndex = 0;
+        else if (axis == Axis.y) axisIndex = 1;
+        else axisIndex = 2;
+
+        lastTrackerRot = rotatingTracker.transform.localEulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(rotatingTracker.transform.localEulerAngles.y);
+
+        //Vector3 rot = transform.localEulerAngles;
+        //rot[axisIndex] = rotFactor * rotatingTracker.transform.localEulerAngles.y;
+        //transform.localEulerAngles = rot;
+        float currentTrackerRot = rotatingTracker.transform.localEulerAngles.y;
+
+        Vector3 rot = new Vector3();
+        int p = 0;
+        if (currentTrackerRot < 10 || currentTrackerRot > 350) p = 60;
+        rot[axisIndex] = (((currentTrackerRot + p) % 360 - (lastTrackerRot + p) % 360) % 360) * rotFactor;
+
+        transform.Rotate(rot);
+
+        lastTrackerRot = currentTrackerRot;
+        
+    }
+}
